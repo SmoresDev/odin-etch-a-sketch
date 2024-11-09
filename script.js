71d34a090d8dd7afb4fc08126a5682cc89faca49
Erin Dinsmore <smorewebdev@gmail.com>
2024-11-09 16:21:02 -0500

    Added a check to make sure user input <100 genBox Added New Grid button with event listener to call genBox

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let boxesContainer = document.getElementById('sketch-boxes');
+let eraseBtn = document.getElementById('erase');
 
 function genBoxes () {
-    while (boxesContainer.firesChild) {
+    while (boxesContainer.firstChild) {
         boxesContainer.removeChild(boxesContainer.lastChild)
     }
     let numBoxes = prompt("How many boxes per side?\nAny number lower than 100");
+    while (numBoxes > 100) {
+        numBoxes = prompt(`Error ${numBoxes} is not a number smaller than 100\nPlease enter a number lower than 100`);
+    }
     boxesContainer.style.maxWidth = `${numBoxes*25}px`
     for (let i = 0; i < (Number(numBoxes)*Number(numBoxes)); i++){
         let newBox = document.createElement('div');
@@ -16,13 +20,13 @@ function genBoxes () {
 function colorBoxes (targetBox) {
     if (targetBox.class = 'box' && targetBox.shiftKey == true) {
         console.log(targetBox)
-        targetBox.target.style.backgroundColor = 'black';
+        targetBox.target.style.backgroundColor = 'black'
     } else if (targetBox.class = 'box' && targetBox.ctrlKey == true){
         targetBox.target.style.backgroundColor = "white";
     }
     
 }
 
-genBoxes()
-
-boxesContainer.addEventListener('mouseover', colorBoxes)
+genBoxes();
+boxesContainer.addEventListener('mouseover', colorBoxes);
+eraseBtn.addEventListener('click', genBoxes);
