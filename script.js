65203f33ecf635104697c8b28b2c1bb4db2fc11d
Erin Dinsmore <smorewebdev@gmail.com>
2024-11-09 15:03:06 -0500

    Created function that generates N number of boxes from user prompt

---
+++ script.js
@@ -0,0 +1,12 @@
+let boxesContainer = document.getElementById('sketch-boxes');
+
+function genBoxes () {
+    let numBoxes = prompt("How many boxes would you like to draw with?\nAny number between 4 and 100");
+    for (let i = 0; i < Number(numBoxes); i++){
+        let newBox = document.createElement('div');
+        newBox.classList.add('box');
+        boxesContainer.appendChild(newBox);
+    }
+}
+
+genBoxes()
